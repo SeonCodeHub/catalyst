159382f88a003d3bfb384e0ca105ed4fbd2c27d8
Talgar <talgarmarlis@gmail.com>
2024-03-14 18:33:07 +0100

    Spring initialized fresh setup repo

---
+++ src/main/java/com/seon/catalyst/CatalystApplication.java
@@ -0,0 +1,13 @@
+package com.seon.catalyst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalystApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalystApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seon/catalyst/CatalystApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seon.catalyst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalystApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
