cfffabd00e5855234c043fd599da0e5e1cdcb36f
Talgar <talgarmarlis@gmail.com>
2024-03-18 19:29:15 +0100

    commit to test pre-commit

--- src/main/java/com/seon/catalyst/CatalystApplication.java
+++ src/main/java/com/seon/catalyst/CatalystApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CatalystApplication {
+
     
     public static void main(final String[] args) {
         SpringApplication.run(CatalystApplication.class, args);
