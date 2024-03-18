08e4dd453cedcfa5cac2a79eac3263f45cb5b3b8
Talgar <talgarmarlis@gmail.com>
2024-03-18 19:33:46 +0100

    pre-commit hook finalized

--- src/main/java/com/seon/catalyst/CatalystApplication.java
+++ src/main/java/com/seon/catalyst/CatalystApplication.java
@@ -6,7 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CatalystApplication {
 
-    
+    /**
+     * Main method to start the Catalyst application.
+     *
+     * @param args The command line arguments passed to the application.
+     */
     public static void main(final String[] args) {
         SpringApplication.run(CatalystApplication.class, args);
     }
